07cbcb77e61d468f30aafd1e0dd0c1f6b54eb457
Stefan <stefan.holmberg@systementor.se>
2023-01-25 17:49:52 +0100

    Add project files.

---
+++ csharphashpwddemo/App.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public class App
+{
+    private readonly string dictFile = "example.dict.txt";
+    private readonly string hashedFile = "hashadelosen.txt";
+
+    public void Run()
+    {
+        EnsurePreHashedFileExists();
+        while (true)
+        {
+            Console.WriteLine("Ange hash:");
+            var hash = Console.ReadLine();
+            //TODO Skriv kod som söker i hashadelosen.txt och 
+            //Hittar du denna hash så är ju lösenordet på samma rad!
+        }
+    }
+
+    private void EnsurePreHashedFileExists()
+    {
+        if (File.Exists(hashedFile)) return;
+
+        var writer = File.CreateText(hashedFile);
+        foreach (var line in File.ReadLines(dictFile))
+        {
+            var hash = GenerateHash(line);
+            writer.WriteLine(hash + " " + line);
+        }
+        writer.Close();
+    }
+
+    private string GenerateHash(string line)
+    {
+        using (var md5 = MD5.Create())
+        {
+            return BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(line)))
+                .Replace("-", "");
+        }
+    }
+}
